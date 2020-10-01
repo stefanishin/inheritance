d1ae9c7913649ec0a573328988572cd40915a846
Stefanishin Mikita <stefanishin92@gmail.com>
2020-10-01 19:25:02 +0300

    add class Triangle

---
+++ ConsoleAppGeomFigure/ConsoleAppGeomFigure/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGeomFigure
+{
+    // абстрактный класс фигуры
+    abstract class Figure
+    {
+        // абстрактный метод для получения периметра
+        public abstract double Perimeter();
+        // абстрактный метод для получения площади
+        public abstract double Area();
+    }
+    // производный класс треугольника
+    class Triangle : Figure
+    { 
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public Triangle(double a, double b, double c)
+        {
+            this.A = a;
+            this.B = b;
+            this.C = c;
+        }
+        // переопределение получения площади
+        public override double Area()
+        {//Формула Герона (Heron's formula)
+            double s = Perimeter() / 2; // полупериметр (semi-perimeter)
+            return Math.Sqrt(s * (s - A) * (s - B) * (s - C));
+        }
+        // переопределение получения периметра
+        public override double Perimeter()
+        {
+            return A + B + C;
+        }
+
+        public override string ToString()
+        {
+            return "Triangle";
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($" Периметр треугольника равен {Perimeter()}/n Площадь треугольник равен {Area()}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
